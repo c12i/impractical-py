c2ebd012c108fd60b8280b9139afa9bc2eedcf9b
Collins Muriuki <murerwacollins@gmail.com>
2020-10-27 12:33:06 +0300

    phrase_anagrams: main function

--- anagrams/phrase_anagrams.py
+++ anagrams/phrase_anagrams.py
@@ -33,7 +33,6 @@ def find_anagrams(name, word_list):
         if Counter(test) == word_letter_map:
             anagrams.append(word)
     print(*anagrams, sep="\n")
-    print("*" * 100)
     print(f"Remaining letters = {name}")
     print(f"Number of remaining letters = {len(name)}")
     print(f"Number of remaining real world anagrams = {len(anagrams)}")
@@ -78,7 +77,39 @@ def process_choice(name):
 
 
 def main():
-    pass
+    """
+    Helps user build anagram phrase from their name
+    """
+    name = "".join(initial_name.lower().split())
+    name = name.replace("-", "")
+    limit = len(name)
+    phrase = ""
+    running = True
+
+    while running:
+        temp_phrase = phrase.replace(" ", "")
+
+        if len(temp_phrase) < limit:
+            print(f"Length of anagram phrase = {len(temp_phrase)}")
+
+            find_anagrams(name, dict_file)
+            print("Current anagram phrases = ", end=" ")
+            print(phrase, file=sys.stderr)
+
+            choice, name = process_choice(name)
+            phrase += choice + ""
+        elif len(temp_phrase) == limit:
+            print("\n *****FINISHED*****\n")
+            print("Anagram of name=", end=" ")
+            print(phrase, file=sys.stderr)
+            print()
+
+            try_again = input("\n\nTry again? (Press Enter else 'n' to quit)\n")
+            if try_again.lower() == "n":
+                running = False
+                sys.exit()
+            else:
+                main()
 
 
 if __name__ == '__main__':
