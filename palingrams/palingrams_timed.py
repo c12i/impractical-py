23207ec66fd542ba50a16695e98392b2712f9923
Collins Muriuki <hello@collinsmuriuki.xyz>
2022-03-31 23:48:40 +0300

    Prefer optimized version

--- palingrams/palingrams_timed.py
+++ palingrams/palingrams_timed.py
@@ -6,10 +6,10 @@ Runs find_palingrams.
 Timing performance with the time module
 """
 import time
-import palingrams
+import palingrams_optimized
 
 start_time = time.time()
-palingrams = palingrams.find_palingrams()
+palingrams = palingrams_optimized.find_palingrams()
 end_time = time.time()
 print(palingrams)
 print(f"Runtime for this program was {end_time - start_time} seconds")
